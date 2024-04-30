ff6fdbef669dcd37a841e99732d70474677e7f51
Vinu Devaraj <vinu.devaraj@logidots.com>
2024-04-30 14:57:44 +0530

    chore: new comit

--- src/components/EngageSpot.tsx
+++ src/components/EngageSpot.tsx
@@ -5,6 +5,7 @@ const EngageSpot = ({ text }: { text: string }) => {
   return (
     <View>
       <Text>{text}</Text>
+      <Text>Great package</Text>
       <Image
         source={{
           uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
